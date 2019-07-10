6b23a082a251ca19a4b432bcebd710faa3a13cc0
Mariano <leonardo.h.mariano@hotmail.com>
2019-07-10 20:51:56 -0300

    aula 1 vue

--- 1-javascript-fundamentos/4-functions/js/main.js
+++ 1-javascript-fundamentos/4-functions/js/main.js
@@ -6,14 +6,35 @@
   full-name
 */
 
-function minhaFuncao(name) {
-  return `Olá, ${name}`
-}
+const funcaoSoma = (n1,n2) => `Soma = ${n1+n2}`   
+
+const funcaoSub = (n1,n2) => `Subtração = ${n1-n2}`  
+
+const funcaoMult = (n1,n2) => `Multiplicação = ${n1*n2}`  
+
+const funcaoDiv = (n1,n2) => `Divisão = ${n1/n2}`  
+
+const funcaoFullName = (primeiroNome,segundoNome) => `Nome completo = ${primeiroNome +" "+ segundoNome}`  
+
+
+console.log(funcaoSoma(1,2))
+
+console.log(funcaoSub(10,2))
+
+console.log(funcaoMult(10,5))
+
+console.log(funcaoDiv(100,5))
+
+console.log(funcaoFullName('Leonardo', 'Mariano'))
+
+
+// function minhaFuncao(name) {
+//   return `Olá, ${name}`
+// }
 
 // const minhaFuncao = function (name) {
 //   return `Olá, ${name}`
 // }
 
-// const minhaFuncao = name => `Olá, ${name}`
+//const minhaFuncao = name => `Olá, ${name}`
 
-console.log(minhaFuncao())
--- 1-javascript-fundamentos/6-condicionais/js/main.js
+++ 1-javascript-fundamentos/6-condicionais/js/main.js
@@ -4,14 +4,36 @@ const c = '5'
 
 /*
   Operadores de comparação
-  - igual a
-  - identico a 
+  - igual a ==
+  - identico a ===
   - diferente
   - maior que
   - maior ou igual a
   - menor que
   - menor ou igual a
 */
+
+console.log('igual', b == c)
+
+console.log('identico', b === c)
+
+console.log('diferente', b != c)
+
+console.log('realmente é diferente', b !== c)
+
+console.log('maior', a > b)
+
+console.log('maior ou igual', a >= b)
+
+console.log('menor', a < b)
+
+console.log('menor ou igual', a <= b)
+
+if (4 > 2){
+  console.log('é')
+}else{
+  console.log('não é')
+}
 
 
 /*
